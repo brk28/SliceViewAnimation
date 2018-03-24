8755994334eff4a11e972fb84dbabc9baae7b63b
Burak Ceylan <burak28@me.com>
2018-03-24 14:14:32 +0100

    WIP

--- SliceViewAnimation/ViewController.swift
+++ SliceViewAnimation/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var containerView: UIView!
+	
+	var view1: UIView?
+	var view2: UIView?
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func button(_ sender: Any) {
+		let firstRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 2.0)
+		let secondRect = CGRect(x: 0, y: view.frame.size.height/2.0, width: view.frame.size.width, height: view.frame.size.height/2.0)
+		view1 = containerView.resizableSnapshotView(from: firstRect, afterScreenUpdates: true, withCapInsets: .zero)
+		view2 = containerView.resizableSnapshotView(from: secondRect, afterScreenUpdates: true, withCapInsets: .zero)
+		view2?.frame.origin.y = view.frame.height / 2.0
+		containerView.isHidden = true
+		view.addSubview(view1!)
+		view.addSubview(view2!)
+		UIView.animate(withDuration: 0.5) {
+			self.view1?.transform = CGAffineTransform(translationX: 0, y: -self.view.frame.height / 2.0)
+			self.view2?.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height / 2.0)
+		}
 	}
-
+	
+	@IBAction func close(_ sender: Any) {
+		UIView.animate(withDuration: 0.5, animations: {
+			self.view1?.transform = .identity
+			self.view2?.transform = .identity
+		}) { (success) in
+			self.containerView.isHidden = false
+			self.view1?.removeFromSuperview()
+			self.view2?.removeFromSuperview()
+		}
+	}
+	
 
 }
 
